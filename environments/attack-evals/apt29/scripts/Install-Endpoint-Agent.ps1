a38e70a264d8a55c1d96ebc816a7c5417b6c7cda
ashdsetty <63748134+ashdsetty@users.noreply.github.com>
2020-11-06 16:42:20 -0500

    Update Install-Endpoint-Agent.ps1

--- environments/attack-evals/apt29/scripts/Install-Endpoint-Agent.ps1
+++ environments/attack-evals/apt29/scripts/Install-Endpoint-Agent.ps1
@@ -5,7 +5,7 @@ invoke-webrequest -uri $uri -outfile "c:\windows\temp\cagent-windows-x64.exe";
 $uri = "https://www.dropbox.com/s/a0bhndzvbb8elhn/etw_logger.zip?dl=1";
 invoke-webrequest -uri $uri -outfile "c:\windows\temp\etw_logger.zip";
 
-Expand-Archive "c:\windows\temp\etw_logger.zip" -DestinationPath "c:\program files\cylerian\sensors\lmao"
+Expand-Archive "c:\windows\temp\etw_logger.zip" -DestinationPath "c:\program files\cylerian\sensors\etw_logger"
 
 $uri = "https://www.dropbox.com/s/uyxziatvg4whzac/overrides.json?dl=1";
 invoke-webrequest -uri $uri -outfile "c:\program files\cylerian\overrides.json";
