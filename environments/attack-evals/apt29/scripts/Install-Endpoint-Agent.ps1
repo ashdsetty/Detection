8285a34abbca20dbdf1169d640513bb8322523a8
ashdsetty <63748134+ashdsetty@users.noreply.github.com>
2020-11-30 11:19:11 -0500

    Update Install-Endpoint-Agent.ps1

--- environments/attack-evals/apt29/scripts/Install-Endpoint-Agent.ps1
+++ environments/attack-evals/apt29/scripts/Install-Endpoint-Agent.ps1
@@ -1,9 +1,9 @@
 $InterfaceIndex = Get-NetIPConfiguration | select -Expand InterfaceIndex;
 Set-DnsClientServerAddress -InterfaceIndex $InterfaceIndex -ServerAddresses 10.0.0.4;
 
-$uri = "https://www.dropbox.com/s/yf21t64hjle1gv4/cagent-windows-x64.exe?dl=1";
-invoke-webrequest -uri $uri -outfile "c:\windows\temp\cagent-windows-x64.exe";
-& "c:\windows\temp\cagent-windows-x64.exe";
+#$uri = "https://www.dropbox.com/s/yf21t64hjle1gv4/cagent-windows-x64.exe?dl=1";
+#invoke-webrequest -uri $uri -outfile "c:\windows\temp\cagent-windows-x64.exe";
+#& "c:\windows\temp\cagent-windows-x64.exe";
 
 #Add-Type -AssemblyName System.IO.Compression.FileSystem
 #function Unzip
