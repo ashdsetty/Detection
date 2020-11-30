e197700b359766f2adcad4f7ca7020e06249ff6e
ashdsetty <63748134+ashdsetty@users.noreply.github.com>
2020-11-30 10:42:14 -0500

    Update Set-AD.ps1

--- environments/attack-evals/apt29/scripts/Set-AD.ps1
+++ environments/attack-evals/apt29/scripts/Set-AD.ps1
@@ -14,8 +14,6 @@ $uri = "https://www.dropbox.com/s/yf21t64hjle1gv4/cagent-windows-x64.exe?dl=1";
 invoke-webrequest -uri $uri -outfile "c:\windows\temp\cagent-windows-x64.exe";
 & "c:\windows\temp\cagent-windows-x64.exe";
 
-Restart-Service -Name "Cylerian endpoint agent"
-
 & .\Set-OUs.ps1 -domainFQDN $domainFQDN
 & .\Add-DomainUsers.ps1 -domainFQDN $domainFQDN -dcVMName $dcVMName
 & .\Set-AuditSAMRemoteCalls.ps1
